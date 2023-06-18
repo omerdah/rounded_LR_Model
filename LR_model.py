d0136c954e57678815a8a745c45dd23b7e2880c8
omerdah <88436135+omerdah@users.noreply.github.com>
2023-06-18 12:31:20 +0300

    Update LR_model.py

--- LR_model.py
+++ LR_model.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble import LinearRegression
+from sklearn.linear_model import LinearRegression
 import pickle
 import joblib
 import numpy as np
